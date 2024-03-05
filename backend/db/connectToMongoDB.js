9cf269d4c405b196a9a45e59031c9d3ce0b32c1b
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-05 13:51:45 +0100

    Feat:MongoDB Setup

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error.message);
+  }
+};
+
+export default connectToMongoDB;
