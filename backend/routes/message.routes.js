bc00acf2a1ad9ff0959f64ea4c091e53b418cce5
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-06 19:57:49 +0100

    Feat:Protect Route Middleware

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,10 @@
 // message.routes.js
 import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/send/:id", sendMessage); // Remove the "/api/messages" prefix
+router.post("/send/:id", protectRoute, sendMessage); // Remove the "/api/messages" prefix
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // server.js
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with JSON payloads  (from req.body)
+app.use(cookieParser()); // to parse the incoming request with cookies (from req.cookies)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes); // Change this to "/api/messages"
