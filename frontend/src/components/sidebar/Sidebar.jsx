ea719a76d9e1cd366fd7c47022322b36688b30cb
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-08 00:31:39 +0100

    Feat:Create Home

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import SearchInput from "./SearchInput";
+
+const Sidebar = () => {
+  return (
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+      <SearchInput />
+    </div>
+  );
+};
+export default Sidebar;
