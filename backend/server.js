1f213385546d8785db3781fffaeb4e9aa3fffb08
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-07 12:25:06 +0100

    Feat:Get Users for Sidebar Endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -16,11 +19,12 @@ app.use(cookieParser()); // to parse the incoming request with cookies (from req
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes); // Change this to "/api/messages"
+app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Hello World !!");
-});
+// app.get("/", (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send("Hello World !!");
+// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
