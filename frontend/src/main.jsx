9da3b28adb9c34c2f140ad7385b1f60315dc651f
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-11 00:11:25 +0000

    Feat:Add Routes and BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter from react-router-dom
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
