fcce8a19b6abd7da602ea444c5217a5fe1b175d9
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-12 00:49:12 +0000

    Feat:Use AuthContext and Navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"; // Import Routes and Route from react-router-dom
+import { Navigate, Routes, Route } from "react-router-dom"; // Import Routes and Route from react-router-dom
 import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={authUser ? <Navigate to="/" /> : <Home />} />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
       </Routes>
       <Toaster />
     </div>
