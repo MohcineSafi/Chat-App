5e425e7dc5cb1e25ced19c14cbc9128314aad554
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-12 00:46:37 +0000

    Feat:Use AuthContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter from react-router-dom
 import "./index.css";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 // eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
